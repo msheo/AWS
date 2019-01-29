d561ffbfc7e3feb6e42670447347a1e9a4eed356
msheo <alstjr0364@gmail.com>
2019-01-29 14:05:43 +0900

    HARD CORDING

--- server.sh
+++ server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 # You need to install the AWS Command Line Interface from http://aws.amazon.com/cli/
-AMIID=$(aws ec2 describe-images --filters "Name=description, Values=Amazon Linux AMI 2015.03.? x86_64 HVM GP2" --query "Images[0].ImageId" --output text)
-VPCID=$(aws ec2 describe-vpcs --filter "Name=isDefault, Values=true" --query "Vpcs[0].VpcId" --output text)
-SUBNETID=$(aws ec2 describe-subnets --filters "Name=vpc-id, Values=$VPCID" --query "Subnets[0].SubnetId" --output text)
+AMIID=ami-0080e4c5bc078760e
+VPCID=vpc-09d73cfe6cbe2bb5b
+SUBNETID=subnet-041dd066d8a2d395d
 SGID=$(aws ec2 create-security-group --group-name mysecuritygroup --description "My security group" --vpc-id $VPCID --output text)
 aws ec2 authorize-security-group-ingress --group-id $SGID --protocol tcp --port 22 --cidr 0.0.0.0/0
 INSTANCEID=$(aws ec2 run-instances --image-id $AMIID --key-name mykey --instance-type t2.micro --security-group-ids $SGID --subnet-id $SUBNETID --query "Instances[0].InstanceId" --output text)
