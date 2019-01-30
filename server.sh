a956ef648ecfa0a6a862d6cf362babb1147e3c70
msheo <alstjr0364@gmail.com>
2019-01-30 17:03:53 +0900

    Final version of Provisioning  Ubuntu 18.04 LTS

--- server.sh
+++ server.sh
@@ -1,13 +1,26 @@
 #!/bin/bash -e
 # You need to install the AWS Command Line Interface from http://aws.amazon.com/cli/
-AMIID=ami-0080e4c5bc078760e
-VPCID=vpc-09d73cfe6cbe2bb5b
-SUBNETID=subnet-041dd066d8a2d395d
+
+# Recent formal Ubuntu 18.04 LTS bioni
+AMIID=$(aws ec2 describe-images --filters "Name=name, Values=ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20180912" --query Images[0].ImageId --output text)
+
+
+VPCID=$(aws ec2 describe-vpcs --query Vpcs[0].VpcId --output text)
+
+
+SUBNETID=$(aws ec2 describe-subnets --filters "Name=vpc-id, Values=$VPCID" --query "Subnets[0].SubnetId" --output text)
+
+
 SGID=$(aws ec2 create-security-group --group-name mysecuritygroup --description "My security group" --vpc-id $VPCID --output text)
 aws ec2 authorize-security-group-ingress --group-id $SGID --protocol tcp --port 22 --cidr 0.0.0.0/0
+
+
+
 INSTANCEID=$(aws ec2 run-instances --image-id $AMIID --key-name mykey --instance-type t2.micro --security-group-ids $SGID --subnet-id $SUBNETID --query "Instances[0].InstanceId" --output text)
 echo "waiting for $INSTANCEID ..."
 aws ec2 wait instance-running --instance-ids $INSTANCEID
+
+
 PUBLICNAME=$(aws ec2 describe-instances --instance-ids $INSTANCEID --query "Reservations[0].Instances[0].PublicDnsName" --output text)
 echo "$INSTANCEID is accepting SSH connections under $PUBLICNAME"
 echo "ssh -i mykey.pem ec2-user@$PUBLICNAME"
